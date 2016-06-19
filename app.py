167875817902c82177e9f9bfde3227c614059e9c
Dmitry Chaplinsky <dchaplinsky@conversionscience.co.uk>
2016-06-20 02:51:19 +0300

    Basic autodocumenting implemented, example added to app.py

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
-from aiohttp import web
+"""
+Collection of microservices on top of tokenize-uk python module.
 
+tokenize-uk uses regexes and rules to tokenize text into paragraphs,
+paragraphs into sentences and sentences into words.
+"""
+from aiohttp import web
 from validator import validate
 from swagger import document
 from tokenize_uk import tokenize_text
 from schema import TOKENIZE_TEXT_OUTPUT_SCHEMA, TOKENIZE_TEXT_INPUT_SCHEMA
+
+__author__ = "Dmitry Chaplinsky"
+__copyright__ = "Copyright 2016, Dmitry Chaplinsky"
+__credits__ = ["Dmitry Chaplinsky", "Vsevolod Dyomkin"]
+__license__ = "MIT"
+__version__ = "1.0.1"
+__maintainer__ = "Dmitry Chaplinsky"
+__email__ = "chaplinsky.dmitry@gmail.com"
 
 
 @validate(
@@ -11,12 +24,23 @@ from schema import TOKENIZE_TEXT_OUTPUT_SCHEMA, TOKENIZE_TEXT_INPUT_SCHEMA
     output_schema=TOKENIZE_TEXT_OUTPUT_SCHEMA,
 )
 async def tokenize_text_handler(request, *args):
+    """
+    Tokenize given text into paragraphs, sentences and words.
+
+    Endpoint uses Newline method to tokenize paragraphs and regexes and rules
+    to tokenize sentences and words
+    """
     return tokenize_text(request["text"])
 
 
 app = web.Application()
 app.router.add_route("POST", "/tokenize_text", tokenize_text_handler)
-app = document(app)
+
+app = document(
+    app,
+    basePath="/",
+    host="127.0.0.1:8080"
+)
 
 if __name__ == '__main__':
     web.run_app(app)
--- swagger.py
+++ swagger.py
@@ -1,3 +1,5 @@
+import sys
+import inspect
 from collections import defaultdict
 from aiohttp import web
 
@@ -6,15 +8,55 @@ def _convert_jsonschema_to_swagger(jsnschema):
     pass
 
 
-def document(app):
+def parse_docstring(s):
+    title = []
+    description = []
+    strings = inspect.cleandoc(s).split("\n")
+
+    for i, x in enumerate(strings):
+        if x:
+            title.append(x)
+        else:
+            if title:
+                description = strings[i + 1:]
+                break
+            else:
+                continue
+
+    return {
+        "title": " ".join(title),
+        "description": " ".join(description)
+    }
+
+
+def document(app, parse_module_info=True, **kwargs):
+    """
+    Parse information from given app, it's path handlers and callee module
+    itself to populate swagger definition file
+
+    Any part of swagger definition can be overriden with kwargs
+    """
+
     async def swagger_handler(request):
+        """
+        Request handler to serve swagger json config for given app
+        """
         return web.json_response(app["_swagger_config"])
 
-    swagger_config = defaultdict(dict)
-
+    # Populate configs for paths from app routes
+    paths_config = defaultdict(dict)
     for r in app.router.routes():
+        # Title and description are retrieved from docstring of a handler
+        docstrings = parse_docstring(r.handler.__doc__ or "")
+        if len(docstrings["title"]) > 120:
+            # TODO: logging
+            docstrings["title"] = docstrings["title"][:120]
+
+        # Some information is just hardcoded as we have strict requirements
+        # to input and output format of ws endpoints
         desc = {
-            "description": r.handler.__doc__,
+            "summary": docstrings["title"],
+            "description": docstrings["description"],
             "consumes": ["application/json"],
             "produces": ["application/json"],
             "responses": {
@@ -23,10 +65,66 @@ def document(app):
                 "405": {"description": "Method is not allowed"},
             }
         }
-        # print(r.handler.__doc__)
-        # print(getattr(r.handler, "_input_schema"))
-        # print(getattr(r.handler, "_output_schema"))
-        swagger_config[r._resource._path][r.method.lower()] = desc
+        paths_config[r._resource._path][r.method.lower()] = desc
+
+    # Populate complete config
+
+    # Get optional information that overrides info part of the config
+    swagger_config_info = kwargs.pop("info", {})
+    # Some sane defaults
+    swagger_config = {
+        "swagger": "2.0",
+        "schemes": ["http"],
+        "paths": paths_config
+    }
+
+    # That can be overriden with kwargs
+    swagger_config.update(kwargs)
+
+    # Some sane defaults for info part of the config
+    swagger_config_info_default = {
+        "version": "1.0.0",
+    }
+
+    # Which we now can try to extend using introspection, docstrings and
+    # global variables of the main module of the app
+    if parse_module_info:
+        try:
+            caller = inspect.currentframe().f_back
+            mod = sys.modules[caller.f_globals['__name__']]
+
+            # Try to pull information from docstrings
+            swagger_config_info_default.update(parse_docstring(mod.__doc__))
+
+            # As well as global variables
+            if hasattr(mod, "__version__"):
+                swagger_config_info_default["version"] = mod.__version__
+
+            if hasattr(mod, "__license__"):
+                swagger_config_info_default["license"] = {
+                    "name": mod.__license__
+                    # TODO: add urls for well-known licenses
+                }
+
+            # And map it to swagger definitions
+            contact_info = {}
+            if hasattr(mod, "__author__"):
+                contact_info["name"] = mod.__author__
+            if hasattr(mod, "__maintainer__"):
+                contact_info["name"] = mod.__maintainer__
+            if hasattr(mod, "__email__"):
+                contact_info["email"] = mod.__email__
+
+            if contact_info:
+                swagger_config_info_default["contact"] = contact_info
+        except:
+            # TODO: logging
+            pass
+
+    swagger_config["info"] = swagger_config_info_default
+
+    # override it with data from kwargs["info"]
+    swagger_config["info"].update(swagger_config_info)
 
     app["_swagger_config"] = swagger_config
 
@@ -34,4 +132,4 @@ def document(app):
 
     return app
 
-__ALL__ = ["document", ]
+__all__ = ["document", ]
